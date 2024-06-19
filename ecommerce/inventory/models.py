0b2ac95f4bcb75357a1b3f52678835158bfd2722
Okan Hollander <o.hollander1990@gmail.com>
2024-06-19 10:34:46 +0200

    updated datetime models

--- ecommerce/inventory/models.py
+++ ecommerce/inventory/models.py
@@ -9,7 +9,8 @@ class Product(models.Model):
     slug = models.SlugField()
     description = models.TextField()
     is_digital = models.BooleanField()
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
     is_active = models.BooleanField()
 
 
